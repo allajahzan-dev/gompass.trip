e5f5fff24070ac7ae49715a4eeea74d497fc4fee
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2025-10-19 17:18:31 +0530

    patch: corrected the height of the image-3

--- src/app/packages/[title]/components/ImagesSection.tsx
+++ src/app/packages/[title]/components/ImagesSection.tsx
@@ -14,7 +14,7 @@ export default function ImagesSection({ pkg }: Props) {
 
     return (
         <section
-            className="max-h-[90vh] px-5 pb-16 bg-white grid grid-cols-1 gap-5
+            className="min-h-screen px-5 pb-16 bg-white grid grid-cols-1 gap-5
             md:px-10 xl:px-24 md:pb-24 lg:grid-cols-3"
         >
             <div
