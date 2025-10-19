3cb3194eff319c32c5dbe5d05792b4db5c6f9575
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2025-10-19 16:18:15 +0530

    patch: added server url prefix for images-2

--- src/app/packages/[title]/components/ImagesSection.tsx
+++ src/app/packages/[title]/components/ImagesSection.tsx
@@ -2,53 +2,52 @@ import { IPackage } from "@/app/utils/fetchPackages";
 import { SERVER_URL } from "@/constants/url";
 import Image from "next/image";
 
-// Interface for Props
 interface Props {
     pkg: IPackage;
 }
 
-// Images section
 export default function ImagesSection({ pkg }: Props) {
+    const images = pkg.images || [];
+
+    if (images.length < 3) return null;
+
     return (
-        <>
-            {pkg.images.length > 2 && (
-                <section
-                    className="min-h-screen px-5 py-16 bg-white grid grid-cols-1 gap-5
-                    md:px-10 xl:px-24 md:py-24 lg:grid-cols-3"
-                >
-                    <div className="col-span-1 lg:col-span-2">
-                        {pkg.images[2].url && (
-                            <Image
-                                src={SERVER_URL + pkg.images[2].url}
-                                alt={"pkg-image-3"}
-                                width={1000}
-                                height={1000}
-                                className="h-full w-full object-cover object-center"
-                            />
-                        )}
-                    </div>
-                    <div className="grid grid-rows-2 gap-5">
-                        {pkg.images[3].url && (
-                            <Image
-                                src={SERVER_URL + pkg.images[3].url}
-                                alt={"pkg-image-3"}
-                                width={1000}
-                                height={1000}
-                                className="h-full w-full object-cover object-center"
-                            />
-                        )}
-                        {pkg.images[4].url && (
-                            <Image
-                                src={SERVER_URL + pkg.images[4].url}
-                                alt={"pkg-image-4"}
-                                width={1000}
-                                height={1000}
-                                className="h-full w-full object-cover object-center"
-                            />
-                        )}
-                    </div>
-                </section>
-            )}
-        </>
+        <section
+            className="min-h-screen px-5 py-16 bg-white grid grid-cols-1 gap-5
+            md:px-10 xl:px-24 md:py-24 lg:grid-cols-3"
+        >
+            <div className="col-span-1 lg:col-span-2">
+                {images[2]?.url && (
+                    <Image
+                        src={SERVER_URL + images[2].url}
+                        alt="pkg-image-3"
+                        width={1000}
+                        height={1000}
+                        className="h-full w-full object-cover object-center"
+                    />
+                )}
+            </div>
+
+            <div className="grid grid-rows-2 gap-5">
+                {images[3]?.url && (
+                    <Image
+                        src={SERVER_URL + images[3].url}
+                        alt="pkg-image-4"
+                        width={1000}
+                        height={1000}
+                        className="h-full w-full object-cover object-center"
+                    />
+                )}
+                {images[4]?.url && (
+                    <Image
+                        src={SERVER_URL + images[4].url}
+                        alt="pkg-image-5"
+                        width={1000}
+                        height={1000}
+                        className="h-full w-full object-cover object-center"
+                    />
+                )}
+            </div>
+        </section>
     );
 }
